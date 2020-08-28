e77e510a87eaff76d5dce48d76f6be89ddf87bd3
Cristina Benito <cristinatbenito@hotmail.com>
2020-08-27 22:06:20 -0300

    Updates

---
+++ precache-manifest.7f01662746e74733679a8d0c8119bc68.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "541461704fc82907acf5c0a5204196ba",
+    "url": "/projeto-react/index.html"
+  },
+  {
+    "revision": "5d053dd75e52a5143ef7",
+    "url": "/projeto-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0604d81bf7f495853fc6",
+    "url": "/projeto-react/static/js/2.18501bcf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/projeto-react/static/js/2.18501bcf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d053dd75e52a5143ef7",
+    "url": "/projeto-react/static/js/main.6adcda8c.chunk.js"
+  },
+  {
+    "revision": "84007bdb4b07d6dcd0e6",
+    "url": "/projeto-react/static/js/runtime-main.38f0ab75.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/projeto-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
